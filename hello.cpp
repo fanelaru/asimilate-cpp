d6dad6f7b707d9b545cff649a6106e2f96be6736
Stefan <fanelaru@gmail.com>
2023-04-30 17:41:40 +0000

    al doilea commit

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <cstring>
+
+constexpr char url_name[] = "prp-url://adresa";
+constexpr char url_prefix[] = "prp-url://";
+
+class MyClass {};
 
 int main()
 {
-    std::cout << "Hello Bo$$" << std::endl;
+    const char* str = strstr(url_name, url_prefix);
+
+    if (str == url_name) {
+        str += strlen(url_prefix);
+        std::cout << "String la fel" << std::endl;
+    }
+    else {
+        std::cout << "String diferit" << std::endl;
+    }
+
+    const char arr[] = "giginel";
+
+    for (const char& c : arr)
+    {
+        std::cout << c;
+    }
+
+    MyClass* obj = new MyClass();
+
+    MyClass** objArray = new MyClass*[10]();
+
+    *objArray = obj;
 }
