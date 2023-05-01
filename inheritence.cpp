f203fbc44f7b49505e3401134d514cc3046d97d4
Stefan <fanelaru@gmail.com>
2023-05-01 17:45:08 +0000

    Inca 2 clase

--- inheritence.cpp
+++ inheritence.cpp
@@ -51,6 +51,55 @@ public:
     }
 };
 
+class Account {
+protected:
+    double balance;
+
+public:
+    Account(double bal) {
+        if(bal > 0) {
+            balance = bal;
+        }
+        else 
+            bal = 0.0;
+    }
+
+    void credit(double amount) {
+        std::cout << "credit Account" << std::endl;
+        balance += amount;
+    }
+
+    void debit(double amount) {
+        if (balance > amount) {
+            balance = balance - amount;
+        }
+        else {
+            std::cout << "Fonduri insuficiente" << std::endl;
+        }
+    }
+
+    double getBalance() {
+        return balance;
+    }
+};
+
+class CheckingAccount : public Account {
+private:
+    double fee;
+
+public:
+        CheckingAccount(double bal, double f) : Account (bal) { 
+            fee = f;
+            std::cout << "ctor CheckingAccount" << std::endl;
+        }
+        void debit(double amount) {
+            if(balance > amount) 
+                balance = balance - amount - fee;
+            else
+                std::cout << "Insufficient funds" << std::endl;
+        }
+};
+
 int main()
 {
     Employee emp1("Fane Babanu", 2300.12);
@@ -64,5 +113,11 @@ int main()
     std::cout << manag1.toString() << std::endl;
 
     std::cout << "Asta e de test" << std::endl;
+
+    CheckingAccount myacct(500, .5);
+    std::cout << myacct.getBalance() << std::endl;
+    myacct.debit(50);
+    std::cout << myacct.getBalance() << std::endl;
+    myacct.credit(150);
     return 0;
 }
